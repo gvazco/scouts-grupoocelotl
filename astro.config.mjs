9437b6d628ae2b2146552b820cac81c825697557
Gustavo Vázco <gustavovazco@gmail.com>
2024-09-10 16:37:23 -0600

    👌 IMPROVE: change astro config site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,20 +4,21 @@ import { loadEnv } from "vite";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 import robotsTxt from "astro-robots-txt";
-const {
-  PUBLIC_WP_URL
-} = loadEnv(process.env.NODE_ENV, process.cwd(), "");
-
+const { PUBLIC_WP_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-estates-course.vercel.app",
+  site: "https://scouts-grupoocelotl.vercel.app",
   image: {
-    domains: [PUBLIC_WP_URL]
+    domains: [PUBLIC_WP_URL],
   },
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), react(), robotsTxt()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+    robotsTxt(),
+  ],
   output: "hybrid",
-  adapter: vercel()
+  adapter: vercel(),
 });
