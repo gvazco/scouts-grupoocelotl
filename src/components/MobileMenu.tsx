765e219ec76529c5c73f5d806781509662978114
Gustavo Vázco <gustavovazco@gmail.com>
2025-05-23 08:30:37 -0600

    👌 IMPROVE: integrate pallete colors

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -14,7 +14,7 @@ export const MobileMenu = ({ children }: Props) => {
         <FontAwesomeIcon icon={faBars} />
       </div>
       {!!open && (
-        <div className="fixed top-0 left-0 max-h-screen overflow-auto w-screen bg-event-horizon">
+        <div className="fixed top-0 left-0 max-h-screen overflow-auto w-screen bg-dark-engine">
           <div
             className="text-3xl fixed top-4 right-3"
             onClick={() => setOpen(false)}
